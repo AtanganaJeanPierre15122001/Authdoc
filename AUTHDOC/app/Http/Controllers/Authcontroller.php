2b5718ae485f43d72627bc95db7de7df82d8e2a4
Emmanuella168 <emmamataga18@gmail.com>
2024-01-05 14:43:54 +0100

    gestion de l interface

--- AUTHDOC/app/Http/Controllers/Authcontroller.php
+++ AUTHDOC/app/Http/Controllers/Authcontroller.php
@@ -7,6 +7,10 @@ use Illuminate\Http\Request;
 class Authcontroller extends Controller
 {
 
+        public function home()
+        {
+        return view('Home.home');
+        }
         public function login()
         {
             return view('auth/login');
@@ -16,4 +20,49 @@ class Authcontroller extends Controller
         {
             return view('auth/signup');
         }
+<<<<<<< Updated upstream
+=======
+
+        public function signupPost(signuprequest $request)
+        {
+            $request1 = $request->validated();
+            $utilisateur = new Utilisateur();
+            $utilisateur->nom = $request1['first_name'];
+            $utilisateur->prenom = $request1['last_name'];
+            $utilisateur->email = $request1['email'];
+            $utilisateur->password = Hash::make($request1['password']);
+            $utilisateur->fonction = 'uti';
+
+            $utilisateur->save();
+
+            return view('user.main')->with('success','Registered successffuly');
+
+
+        }
+
+        public function loginPost(loginrequest $request)
+        {
+            $credentials = $request->validated();
+
+
+            $utilisateur = Utilisateur::where([
+                'email' => $credentials['email'],
+            ])->first();
+            if ($utilisateur && Hash::check($credentials['password'],$utilisateur->password))
+            {
+//                $user = Auth::user();
+                if ($utilisateur->fonction === 'adm') {
+                    $request->session()->regenerate();
+                    return redirect()->intended(route('admin.main'));
+                } else {
+                    return redirect()->intended(route('user.main'));
+                }
+            }
+
+            return redirect()->route('auth.login')->with('error', 'Adresse e-mail ou mot de passe incorrect.');
+
+
+        }
+
+>>>>>>> Stashed changes
 }
--- AUTHDOC/routes/web.php
+++ AUTHDOC/routes/web.php
@@ -14,8 +14,19 @@ use Illuminate\Support\Facades\Route;
 */
 
 
+<<<<<<< Updated upstream
 Route::get('/', function () {return view('Home/home');});
 Route::get('/auth/login',[\App\Http\Controllers\Authcontroller::class, 'login'])->name('login');
 Route::get('/auth/signup',[\App\Http\Controllers\Authcontroller::class, 'signup'])->name('signup');
 
+=======
+Route::get('/', function () {return view('Home.home');});
+Route::get('/login',[\App\Http\Controllers\Authcontroller::class, 'login'])->name('auth.login');
+Route::get('/signup',[\App\Http\Controllers\Authcontroller::class, 'signup'])->name('auth.signup');
+Route::post('/signup',[\App\Http\Controllers\Authcontroller::class, 'signupPost']);
+Route::post('/login',[\App\Http\Controllers\Authcontroller::class, 'loginPost']);
+Route::get('/admin',[\App\Http\Controllers\admincontroller::class, 'admin'])->name('admin.main');
+Route::get('/user',[\App\Http\Controllers\usercontroller::class, 'user'])->name('user.main');
+Route::get('/home',[\App\Http\Controllers\Authcontroller::class, 'home'])->name('Home.home');
+>>>>>>> Stashed changes
 
